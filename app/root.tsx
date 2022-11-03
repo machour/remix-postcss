6d9855d9f7a956ed020ef2c16676ed83391f1303
Mehdi Achour <machour@gmail.com>
2022-11-03 10:01:44 +0100

    Add postcss

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,16 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "~/css/main.css";
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
